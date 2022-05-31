63251aa3bd0858d460ad45cc9dc4737cbf1cde34
karamalqinneh <karamalqinneh@live.com>
2022-06-01 01:07:46 +0300

    modified some routes

--- src/routes/auth-routes/signin.route.js
+++ src/routes/auth-routes/signin.route.js
@@ -4,7 +4,14 @@ const basicAuth = require("../../middlewares/auth/basic.auth");
 
 // signup Function
 function signinController(req, res) {
-  res.status(200).json({ email: req.user.email, token: req.user.token });
+  res.status(200).json({
+    email: req.user.email,
+    token: req.user.token,
+    actions: req.user.actions,
+    jobTitle: req.user.job_title,
+    name: `${req.user.first_name} ${req.user.last_name}`,
+    id: req.user.employee_id,
+  });
 }
 
 module.exports = router.post("/signin", basicAuth, signinController);
